491b5939fd662d5c93519221e99f26d6a8c94b41
Wellington_Fonseca <thyrso.kof@gmail.com>
2023-02-06 21:54:37 -0300

    feat: Increment Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,16 @@
 function Header () {
   return(
-    <h1>HEADER AQUI </h1>
+    <header>
+      <h1>SG Tasks</h1>
+      <label htmlFor="theme" className={`select is-link` }>
+        <select>
+            <option value="op1">op1</option>
+            <option value="op2">op2</option>
+            <option value="op3">op3</option>
+        </select>
+      </label>
+      <button>Add New Task</button>
+    </header>
   )
 }
   
