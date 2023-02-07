5eb405e33403a4b7c23e4c02e767ae9ae9e7deff
Wellington_Fonseca <thyrso.kof@gmail.com>
2023-02-06 21:29:08 -0300

    feat: Implement BrowseRouter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div >
-      INITIAL COMMIT
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={ <h1>HEADER AQUI </h1>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
