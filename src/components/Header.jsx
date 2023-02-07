6e3bfe68b49f6343da37801be2886e702e231aa3
Wellington_Fonseca <thyrso.kof@gmail.com>
2023-02-06 21:57:31 -0300

    feat: Remove unused properties on label in Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ function Header () {
   return(
     <header>
       <h1>SG Tasks</h1>
-      <label htmlFor="theme" className={`select is-link` }>
+      <label>
         <select>
             <option value="op1">op1</option>
             <option value="op2">op2</option>
