f3ba3d64f7fb48ede538018a25f2dd049a0939a9
Wellington_Fonseca <thyrso.kof@gmail.com>
2023-02-11 18:22:38 -0300

    Testando tamanho do Carousel. Commit safe pq vai faltar luz  =/

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,22 +1,25 @@
 import TaskCard from "./TaskCard";
 import  "../css/taskList.css";
 import { motion } from "framer-motion";
-import { useEffect, useRef } from "react";
+import { useContext, useEffect, useRef } from "react";
+import CarouselContext from "../context/CarouselContext";
 
 function TaskList () {
+  const { width, setWidth } = useContext(CarouselContext);
   const carousel = useRef();
 
   const checkLocalStorage = localStorage.hasOwnProperty('form');
   const allTasks = JSON.parse(localStorage.getItem('form'));
 
   useEffect(() => {
-
+    setWidth(carousel.current?.scrollWidth - carousel.current?.offsetWidth )
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
 
   return(
     <motion.main ref={ carousel }  className="task-list" whileTap={{ cursor: "grabbing" }}>
-      <motion.section className="inner-carousel" drag="x">
+      <motion.section className="inner-carousel" drag="x" dragConstraints={{ right: 0, left: -width}}>
         {!checkLocalStorage ? <h1>Tem nao</h1> : 
           allTasks.map((task, index) =>  <TaskCard key={ task.title + index  } task={ task } /> )}
       </motion.section>
--- src/context/CarouselProvider.jsx
+++ src/context/CarouselProvider.jsx
@@ -2,11 +2,11 @@ import React, { useState } from "react";
 import CarouselContext from "./CarouselContext";
 
 function CarouselProvider ({ children }) {
-  const [Carousel, setCarousel] = useState('');
+  const [width, setWidth] = useState('');
 
 const contextValue = {
-    Carousel,
-    setCarousel,
+    width,
+    setWidth,
 };
 
 return (
