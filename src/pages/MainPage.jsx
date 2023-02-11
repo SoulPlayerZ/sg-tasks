f87e1d2838db6167821bc5cd2d979f0e4f13d5f1
Wellington_Fonseca <thyrso.kof@gmail.com>
2023-02-11 15:35:51 -0300

    feat: Create mainPage.css with themes

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -2,6 +2,7 @@ import { useContext, useEffect } from "react";
 import Header from "../components/Header";
 import TaskList from "../components/TaskList";
 import ThemeContext from "../context/ThemeContext";
+import "../css/mainPage.css";
 
 function MainPage () {
   const { setTheme } = useContext(ThemeContext)
