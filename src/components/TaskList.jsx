46113aee8900f042fe728d2b82eabfe7eda83b05
Wellington_Fonseca <thyrso.kof@gmail.com>
2023-02-11 18:02:07 -0300

    feat: Includes useRef in carousel

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,15 +1,21 @@
 import TaskCard from "./TaskCard";
 import  "../css/taskList.css";
 import { motion } from "framer-motion";
+import { useEffect, useRef } from "react";
 
 function TaskList () {
+  const carousel = useRef();
 
   const checkLocalStorage = localStorage.hasOwnProperty('form');
   const allTasks = JSON.parse(localStorage.getItem('form'));
 
+  useEffect(() => {
+
+  }, [])
+
 
   return(
-    <motion.main className="task-list" whileTap={{ cursor: "grabbing" }}>
+    <motion.main ref={ carousel }  className="task-list" whileTap={{ cursor: "grabbing" }}>
       <motion.section className="inner-carousel" drag="x">
         {!checkLocalStorage ? <h1>Tem nao</h1> : 
           allTasks.map((task, index) =>  <TaskCard key={ task.title + index  } task={ task } /> )}
